5c8b3edfd32c8d17421d912f0adcffcf21cf24ac
Timdows <tim@inexpro.nl>
2016-10-24 20:06:17 +0200

    Setup task2

---
+++ Codility/Task2 - OddOccurrencesInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task2___OddOccurrencesInArray
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var array = new[]
+            {
+                9,
+                3,
+                9,
+                3,
+                9,
+                7,
+                9
+            };
+
+            var a = Solution(array);
+        }
+
+        public static int Solution(int[] array)
+        {
+
+            return 0;
+        }
+    }
+}
